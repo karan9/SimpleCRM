9e1ccec219831a371eb283ad13aa125845dcd932
Karan Srivastava <karan.srivastava@protonmail.com>
2017-02-05 11:50:46 +0530

    pug: enable inital pageloader

---
+++ source/js/script-admin.js
@@ -0,0 +1,21 @@
+$(function() {
+    showPageLoader();
+});
+
+function showPageLoader() {
+    var $pageLoaderModal = $("#pageLoaderModal");
+
+    // initiae Loading Modal
+    $pageLoaderModal.modal({
+        backdrop: 'static',  
+        keyboard: false,
+        show: true
+    });
+    // start animating
+    $(".progress-bar").css("width", "100%");
+
+    // hide after 4secs
+    setTimeout(function() {
+       $pageLoaderModal.modal("hide")
+    }, 4000);
+}
